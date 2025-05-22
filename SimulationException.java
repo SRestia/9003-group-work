8d410e84ae8f23b23d13cc6f4e1143445a94b7d8
Andy <doanvietanh03022003@gmail.com>
2025-05-22 23:46:54 +1000

    implement vehicle class

---
+++ SimulationException.java
@@ -0,0 +1,8 @@
+public class SimulationException extends Exception {
+    public SimulationException(String message) {
+        super(message);
+    }
+    public SimulationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
